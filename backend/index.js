08aaf77e8e310d18dc14cb63d18128cc8729589b
OfirTzrik <ofirc36@gmail.com>
2026-07-26 21:18:39 +0300

    update get single note and add note

--- backend/index.js
+++ backend/index.js
@@ -38,14 +38,9 @@ app.get("/api/notes", (request, response) => {
 })
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -72,12 +67,12 @@ app.post("/api/notes", (request, response) => {
     const note = {
         content: body.content,
         important: body.important || false,
-        id: generateId(),
     }
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+        console.log(`New note saved ${savedNote}`)
+    })
 })
 
 const PORT = process.env.PORT
