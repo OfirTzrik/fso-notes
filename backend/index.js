25424098750ed53f9b277058c2880fa544a4918e
OfirTzrik <ofirc36@gmail.com>
2026-07-26 21:22:56 +0300

    fix new Note

--- backend/index.js
+++ backend/index.js
@@ -64,10 +64,10 @@ app.post("/api/notes", (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-    }
+    })
 
     note.save().then(savedNote => {
         response.json(savedNote)
