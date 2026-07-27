2bc7b3b5733eaf75b73080e06987c34f389323e1
OfirTzrik <ofirc36@gmail.com>
2026-07-27 19:09:38 +0300

    add delete and put

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get("/api/notes", (request, response) => {
     })
 })
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
     Note.findById(request.params.id).then(note => {
         if (note) {
             response.json(note)
@@ -45,16 +45,16 @@ app.get("/api/notes/:id", (request, response) => {
             response.status(404).end()
         }
     }).catch(error => {
+        next(error)
         console.log(error)
         response.status(400).send({ error: "malformatted id" })
     })
 })
 
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete("/api/notes/:id", (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
 })
 
 app.post("/api/notes", (request, response) => {
@@ -77,7 +77,40 @@ app.post("/api/notes", (request, response) => {
     })
 })
 
+app.put("/api/notes/:id", (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findById(request.params.id).then(note => {
+        if (!note) {
+            return response.status(404).end()
+        }
+
+        note["content"] = content
+        note["important"] = important
+
+        return note.save().then((updatedNote) => {
+            response.json(updatedNoted)
+        })
+    }).catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" })
+}
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
