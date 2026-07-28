c7a5081c585aa32da14cd9a2f490c4e859be0eca
OfirTzrik <ofirc36@gmail.com>
2026-07-28 14:42:06 +0300

    update note content validation

--- backend/index.js
+++ backend/index.js
@@ -57,24 +57,18 @@ app.delete("/api/notes/:id", (request, response, next) => {
     }).catch(error => next(error))
 })
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing"
-        })
-    }
 
     const note = new Note({
-        content: body.content,
-        important: body.important || false,
+        content: body["content"],
+        important: body["important"] || false,
     })
 
     note.save().then(savedNote => {
         response.json(savedNote)
         console.log(`New note saved ${savedNote}`)
-    })
+    }).catch(error => next(error))
 })
 
 app.put("/api/notes/:id", (request, response, next) => {
@@ -89,7 +83,7 @@ app.put("/api/notes/:id", (request, response, next) => {
         note["important"] = important
 
         return note.save().then((updatedNote) => {
-            response.json(updatedNoted)
+            response.json(updatedNote)
         })
     }).catch(error => next(error))
 })
@@ -108,6 +102,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" })
+    } else if (error.name == "ValidationError") {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
