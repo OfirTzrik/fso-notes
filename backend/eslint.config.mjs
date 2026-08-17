a0412d579e53583f592cc8ab6d9d373ef62ea362
OfirTzrik <ofirc36@gmail.com>
2026-08-17 14:07:14 +0300

    restructure project

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -15,10 +15,18 @@ export default [
             "@stylistic/js": stylisticJs,
         },
         rules: {
-            "@stylistic/js/indent": ["error", 4],
+            "@stylistic/js/indent": ["error", 2],
             "@stylistic/js/linebreak-style": ["error", "unix"],
             "@stylistic/js/quotes": ["error", "single"],
             "@stylistic/js/semi": ["error", "never"],
+            eqeqeq: "error",
+            'no-trailing-spaces': 'error',
+            'object-curly-spacing': ['error', 'always'],
+            'arrow-spacing': ['error', { before: true, after: true }],
+            "no-console": "off",
         },
+    },
+    {
+        ignores: ["dist/**"],
     },
 ]
