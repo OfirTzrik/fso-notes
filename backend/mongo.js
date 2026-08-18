e9519cfeee6eecdd86f70e3434d635699cc900be
OfirTzrik <ofirc36@gmail.com>
2026-08-18 17:17:43 +0300

    re-add mongo.js (required for part4b)

---
+++ backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://ofir:${password}@cluster0.up5gl3f.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'This is a test note and it is not important at all (or is it?)',
+  important: true,
+})
+
+note.save().then(() => {
+  console.log('note saved!')
+  return Note.find({})
+}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
